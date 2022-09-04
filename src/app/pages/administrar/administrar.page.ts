4fbb7b9bae3b5fe143d15a2a23fb2489120fc9e3
Naxhunter <jos.contrerasv@duocuc.cl>
2022-09-04 03:52:49 -0300

    Administracion funcional

--- src/app/pages/administrar/administrar.page.ts
+++ src/app/pages/administrar/administrar.page.ts
@@ -10,6 +10,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./administrar.page.scss'],
 })
 export class AdministrarPage implements OnInit {
+  alumno = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,2}.[0-9]{3}.[0-9]{3}-[0-9kK]{1}')]),
+    nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    apellido: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    correo: new FormControl('',[Validators.email,Validators.required]),
+    fecha_nac: new FormControl('', Validators.required),
+    auto: new FormControl('',Validators.required),
+    password: new FormControl('', [Validators.required, 
+                                   Validators.minLength(6),
+                                   Validators.maxLength(18)]),
+    tipo_usuario: new FormControl('',Validators.required)
+  });
+  
   rut: any;
   sesion: any = [];
   variable: string = "Administrar";
@@ -17,6 +30,8 @@ export class AdministrarPage implements OnInit {
   registrar: any = undefined;
   modificar: any = undefined;
   eliminar: any = undefined;
+  verificar_password: string;
+  usuario_buscado: any = [];
   constructor(private navCtrl:NavController, private route: ActivatedRoute, private usuarioService: UsuarioService) { }
 
   ngOnInit() {
@@ -50,5 +65,46 @@ export class AdministrarPage implements OnInit {
     this.modificar = undefined;
     this.eliminar = 1;
     this.registrar = undefined;
+  }
+  
+  registrarAdmin(){
+    if (this.alumno.controls.password.value != this.verificar_password) {
+      alert('¡CONTRASEÑAS NO COINCIDEN!');
+      return;
+    }
+    this.usuarioService.agregarUsuario(this.alumno.value);
+    this.alumno.reset();
+    alert('¡USUARIO REGISTRADO!');
+  }
+  eliminarAdmin(){
+    this.usuarioService.eliminarUsuario(this.alumno.controls.rut.value);
+    alert('¡USUARIO ELIMINADO!');
+  }
+  buscarAdmin(){
+    this.usuario_buscado = this.usuarioService.obtenerUsuario(this.alumno.controls.rut.value);
+    this.modificar = 2;
+    return this.usuario_buscado;
+  }
+  modificarAdmin(){
+    if (this.alumno.controls.password.value != this.verificar_password) {
+      alert('¡CONTRASEÑAS NO COINCIDEN!');
+      return;
+    }
+    this.usuarioService.actualizarUsuario(this.alumno.value);
+    this.alumno.reset();
+    alert('¡USUARIO ACTUALIZADO!');
+  }
+  
+  perfil(rut){
+    this.navCtrl.navigateForward(['/perfil',rut]);
+  }
+  cerrarSesion(){
+    this.navCtrl.navigateForward(['/login']);
+  }
+  irCrearViaje(){
+    this.navCtrl.navigateForward(['/nuevoviaje']);
+  }
+  irSolicitudViaje(){
+    this.navCtrl.navigateForward(['/solicitud']);
   }
 }
