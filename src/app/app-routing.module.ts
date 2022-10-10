510d4131c5a3f16a35d51050d3c9425ba955bae7
Naxhunter <jos.contrerasv@duocuc.cl>
2022-10-09 22:00:35 -0300

    auth implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,35 +10,41 @@ const routes: Routes = [
   },
   {
     path: 'home/:rut',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
 
   },
   {
     path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule)
   },
   {
     path: 'recuperar',
-    loadChildren: () => import('./pages/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+    loadChildren: () => import('./pages/recuperar/recuperar.module').then(m => m.RecuperarPageModule)
   },
   {
     path: 'administrar/:rut',
-    loadChildren: () => import('./pages/administrar/administrar.module').then( m => m.AdministrarPageModule)
+    loadChildren: () => import('./pages/administrar/administrar.module').then(m => m.AdministrarPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'perfil/:rut',
-    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
-  },  {
+    loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
     path: 'solicitud',
-    loadChildren: () => import('./pages/solicitud/solicitud.module').then( m => m.SolicitudPageModule)
+    loadChildren: () => import('./pages/solicitud/solicitud.module').then(m => m.SolicitudPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'nuevoviaje',
-    loadChildren: () => import('./pages/nuevoviaje/nuevoviaje.module').then( m => m.NuevoviajePageModule)
+    loadChildren: () => import('./pages/nuevoviaje/nuevoviaje.module').then(m => m.NuevoviajePageModule),
+    canActivate: [AuthGuard]
   },
 
 
