774d69da477e72b808508fad7007df6ab63fd0f4
Naxhunter <jos.contrerasv@duocuc.cl>
2022-10-14 03:03:56 -0300

    disponible cargando datos falta irDetalle

--- src/app/pages/disponible/disponible.page.ts
+++ src/app/pages/disponible/disponible.page.ts
@@ -13,26 +13,30 @@ export class DisponiblePage implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private usuarioService: UsuarioService, private storage: StorageService) { }
   usuario: any = [];
   viajes: any = [];
-  conductor: any = [];
+  total: any = [];
   rut: any
   KEY_VIAJE = "viajes";
   KEY_USUARIO = "usuarios";
   async ngOnInit() {
     console.log("Entre en Disponible:");
     let rut = this.route.snapshot.paramMap.get('rut');
-    this.usuario = await this.storage.getDato(this.KEY_USUARIO,rut);
-    console.log("Traigo al usuario en sesión:",this.usuario);
+    this.usuario = await this.storage.getDato(this.KEY_USUARIO, rut);
+    console.log("Traigo al usuario en sesión:", this.usuario);
     this.viajes = await this.storage.getDatos(this.KEY_VIAJE);
-    console.log("Traigo los viajes:",this.viajes);
+    console.log("Traigo los viajes:", this.viajes);
     this.viajes.forEach(async (value, index) => {
       console.log("Entro en el foreach");
       var interna = await this.storage.getDato(this.KEY_USUARIO, value.rut_conductor);
-      this.conductor.push(interna);
-      console.log("Estoy imprimiendo cada dato encontrado:",interna);
+      var precio = value.precio;
+      var arreglo = {
+        precios: precio,
+        dato: interna
+      };
+      this.total.push(arreglo);
     });
-    console.log("Imprimo mis conductores con sus autos:", this.conductor);
+    console.log("Total",this.total);
   }
-  async irDetalle(rut){
-    
+  async irDetalle(rut) {
+
   }
 }
